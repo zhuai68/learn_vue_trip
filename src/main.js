5c04cf9d8b133f0ca9eda9d63e93977b5f34dd03
terrylee <zhuai1101@gmail.com>
2023-03-09 21:00:59 +0800

    1.pinia-store的基本配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
+import pinia from "./store";
 
 import "normalize.css";
 import "./assets/css/index.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(pinia).mount("#app");
